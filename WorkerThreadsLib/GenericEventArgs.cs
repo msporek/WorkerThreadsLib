e2ee034161c45e18a760cbd1cdf88e7036f0f07f
Michal Sporek <msporek@gmail.com>
2023-05-31 09:58:55 +0200

    More comments for GenericEventArgs class.

--- WorkerThreadsLib/GenericEventArgs.cs
+++ WorkerThreadsLib/GenericEventArgs.cs
@@ -9,8 +9,17 @@ namespace WorkerThreadsLib;
 /// <typeparam name="T">Type of data to be carried with the <see cref="GenericEventArgs{T}"/>.</typeparam>
 public class GenericEventArgs<T> : EventArgs
 {
+    /// <summary>
+    /// Data that the instance of event args carries. 
+    /// </summary>
     public T Data { get; set; }
 
+    /// <summary>
+    /// Constructor creates a new instance of <see cref="GenericEventArgs{T}"/> with the data passed as an argument.
+    /// </summary>
+    /// 
+    /// <param name="data">Data to be carried with the event args. 
+    /// This argument can be null / default for <typeparamref name="T"/> type.</param>
     public GenericEventArgs(T data = default(T))
     {
         this.Data = data;
