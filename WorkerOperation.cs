4515ff30ee2440310b91f07333253797b6c56c46
Michal Sporek <msporek@gmail.com>
2023-06-13 16:50:23 +0200

    Handling of WorkerOperation exception.
    Changes in the implementation of the WorkerOperation's Exception (WorkerOperation.OperationException property). 
    Added some additional documentary comments.

---
+++ WorkerOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkerThreadsLib;
+
+/// <summary>
+/// Base class for all operations to be scheduled for execution on instances of <see cref="WorkerThread"/> class.
+/// </summary>
+public abstract class WorkerOperation
+{
+    /// <summary>
+    /// Public parameterless constructor. 
+    /// </summary>
+    /// 
+    /// <remarks>It is is only possible to call it from derived classes, because <see cref="WorkerThread"/> class is abstract.</remarks>
+    public WorkerOperation()
+    {
+    }
+
+    /// <summary>
+    /// Operation to be executed. 
+    /// </summary>
+    public abstract void Run();
+
+    /// <summary>
+    /// Should carry results of operation execution in the case of an error - exception that occured making the operation fail. 
+    /// </summary>
+    public abstract Exception OperationException { get; set; }
+
+    /// <summary>
+    /// Gets or sets value indicating a unique operation key. 
+    /// </summary>
+    public abstract string OperationKey { get; }
+}
